f8b730b4a596082eb4c10d0af7ea49ad640ac0ad
28tiwarir <28tiwarir@bwschools.net>
2024-12-10 08:48:53 -0500

    commit

---
+++ main.py
@@ -0,0 +1,95 @@
+import turtle
+Actual_Deperession = turtle.Screen()
+BIG_MAN_T = turtle.Turtle()
+BIG_MAN_T.speed(0)
+BIG_MAN_Ts_WIFE = turtle.Turtle()
+BIG_MAN_Ts_WIFE.speed(0)
+t = turtle.Turtle()
+t.hideturtle()
+t.penup()
+
+t.speed(0)
+Actual_Deperession.bgcolor("lightblue")
+BIG_MAN_T.hideturtle()
+BIG_MAN_Ts_WIFE. hideturtle()
+BIG_MAN_Ts_WIFE.speed(1)
+BIG_MAN_Ts_WIFE.penup()
+BIG_MAN_Ts_WIFE.goto(-200,100)
+BIG_MAN_Ts_WIFE.pendown()
+
+BIG_MAN_Ts_WIFE. write("Roseate's Domain.", font=("Arial", 40, "normal"))
+BIG_MAN_T.penup()
+BIG_MAN_T. goto(-300, -100)
+BIG_MAN_T.pendown()
+BIG_MAN_T. write("⚠️CLASSIFIED INFO.⚠️", font=("Arial", 40, "normal"))
+
+enter = input("Press Enter to Start the road to my Domain of Information.")
+
+BIG_MAN_T.clear()
+BIG_MAN_Ts_WIFE.clear()
+
+Actual_Deperession.bgcolor("red")
+
+BIG_MAN_Ts_WIFE.penup()
+BIG_MAN_T.penup()
+
+BIG_MAN_T. goto(-200,300)
+BIG_MAN_T. pendown()
+BIG_MAN_T. write("I like rice, dumplings, and ramen", font=("Arial", 20, "normal"))
+BIG_MAN_T. penup()
+
+enter = input("Press Enter to continue the road to learning my likes.")
+
+BIG_MAN_T.clear()
+BIG_MAN_Ts_WIFE.clear()
+
+Actual_Deperession.bgcolor("yellow")
+
+BIG_MAN_T.goto(-300, 100)
+BIG_MAN_T. pendown()
+BIG_MAN_T. write("I like to play soccer, play video games,  sleep, and troll my friends", font=("Arial", 16, "normal"))
+BIG_MAN_T. penup()
+
+enter = input("Press Enter to continue the road to learning my likes.")
+
+BIG_MAN_T.clear()
+BIG_MAN_Ts_WIFE.clear()
+
+Actual_Deperession.bgcolor("grey")
+
+BIG_MAN_T.goto(-300, 100)
+BIG_MAN_T. pendown()
+BIG_MAN_T. write("I like any ben10 movie execpt for the reboots cause they are BANGERS.", font=("Arial", 16, "normal"))
+BIG_MAN_T. penup()
+
+
+enter = input("Press Enter to continue the road to learning my likes.")
+
+BIG_MAN_T.clear()
+BIG_MAN_Ts_WIFE.clear()
+
+Actual_Deperession.bgcolor("purple")
+
+BIG_MAN_T.goto(-200, 100)
+BIG_MAN_T. pendown()
+BIG_MAN_T. write("Soccer is my favorite sport.", font=("Arial", 20, "normal"))
+BIG_MAN_T. penup()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+turtle.done()
